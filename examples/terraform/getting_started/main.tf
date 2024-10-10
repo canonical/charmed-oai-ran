68ca874c625263144711a47af38fd0c63758749a
Bartlomiej Gmerek <42570669+Gmerold@users.noreply.github.com>
2024-10-10 15:12:54 +0200

    fix: Fixes Getting Started tutorial (#12)

--- examples/terraform/getting_started/main.tf
+++ examples/terraform/getting_started/main.tf
@@ -1,27 +1,25 @@
 # Copyright 2024 Canonical Ltd.
 # See LICENSE file for licensing details.
 
-resource "juju_model" "private5g" {
-  name = "private5g"
+resource "juju_model" "sdcore" {
+  name = "sdcore"
 }
 
 module "sdcore-router" {
   source = "git::https://github.com/canonical/sdcore-router-k8s-operator//terraform"
 
-  model_name = juju_model.private5g.name
-  depends_on = [juju_model.private5g]
+  model      = juju_model.sdcore.name
+  depends_on = [juju_model.sdcore]
 }
 
 module "sdcore" {
   source = "git::https://github.com/canonical/terraform-juju-sdcore//modules/sdcore-k8s"
 
-  model_name = juju_model.private5g.name
-  create_model = false
+  model        = juju_model.sdcore.name
+  depends_on = [module.sdcore-router]
 
   traefik_config = {
     routing_mode      = "subdomain"
-    external_hostname = "10.0.0.3.nip.io"
+    external_hostname = "10.0.0.2.nip.io"
   }
-
-  depends_on = [module.sdcore-router]
 }
---
+++ examples/terraform/getting_started/ran.tf
@@ -0,0 +1,82 @@
+# Copyright 2024 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+resource "juju_model" "oai-ran" {
+  name = "ran"
+}
+
+module "cu" {
+  source = "git::https://github.com/canonical/oai-ran-cu-k8s-operator//terraform"
+
+  model_name = juju_model.oai-ran.name
+  config     = {
+    "n3-interface-name": "ran"
+  }
+}
+
+resource "juju_offer" "cu-fiveg-gnb-identity" {
+  model            = juju_model.oai-ran.name
+  application_name = module.cu.app_name
+  endpoint         = module.cu.fiveg_gnb_identity_endpoint
+}
+
+resource "juju_integration" "cu-amf" {
+  model = juju_model.oai-ran.name
+  application {
+    name     = module.cu.app_name
+    endpoint = module.cu.fiveg_n2_endpoint
+  }
+  application {
+    offer_url = module.sdcore.amf_fiveg_n2_offer_url
+  }
+}
+
+resource "juju_integration" "cu-nms" {
+  model = juju_model.sdcore.name
+  application {
+    name     = module.sdcore.nms_app_name
+    endpoint = module.sdcore.fiveg_gnb_identity_endpoint
+  }
+  application {
+    offer_url = juju_offer.cu-fiveg-gnb-identity.url
+  }
+}
+
+module "du" {
+  source = "git::https://github.com/canonical/oai-ran-du-k8s-operator//terraform"
+
+  model_name = juju_model.oai-ran.name
+  config     = {
+    "simulation-mode": true
+  }
+}
+
+resource "juju_integration" "du-cu" {
+  model = juju_model.oai-ran.name
+  application {
+    name     = module.du.app_name
+    endpoint = module.du.fiveg_f1_endpoint
+  }
+  application {
+    name     = module.cu.app_name
+    endpoint = module.cu.fiveg_f1_endpoint
+  }
+}
+
+module "ue" {
+  source = "git::https://github.com/canonical/oai-ran-ue-k8s-operator//terraform"
+
+  model_name = juju_model.oai-ran.name
+}
+
+resource "juju_integration" "ue-du" {
+  model = juju_model.oai-ran.name
+  application {
+    name     = module.ue.app_name
+    endpoint = module.ue.fiveg_rfsim_endpoint
+  }
+  application {
+    name     = module.du.app_name
+    endpoint = module.du.fiveg_rfsim_endpoint
+  }
+}
